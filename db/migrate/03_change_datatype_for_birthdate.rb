011469a41390d216ff95e1119be654c498a6bdfc
Hanna Lee <mani21na@gmail.com>
2019-10-26 23:19:58 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,6 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
-  def change
-    change_column :students, :birthdate, :datetime
-  end
-end
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+  def change 
+    change_column :students, :birthday, :datetime
+  end 
+  
+end 
